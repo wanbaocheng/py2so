855272bb56fe07e98bf9a9f7373ca1787407298d
wanbaocheng <wanbaocheng1@jd.com>
2019-10-27 19:20:32 +0800

    修改及删除文件不支持python2

--- config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-uname -a | grep -i 'ubuntu' > /dev/null
-echo $?
-if [ $? -eq 0 ]; then
-    apt-get install python-dev cython python3-dev cython3
-else
-    yum install python-dev cython python3-dev cython3
-fi
