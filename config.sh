f50c0a10f138789809337dceb837129b762bde3c
wanbaocheng <wanbaocheng1@jd.com>
2019-10-27 18:09:30 +0800

    修改config.sh中的一处bug，不区分ubuntu的大小写

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-uname -a | grep 'ubuntu' > /dev/null
+uname -a | grep -i 'ubuntu' > /dev/null
 echo $?
 if [ $? -eq 0 ]; then
     apt-get install python-dev cython python3-dev cython3
