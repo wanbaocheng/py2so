0b59c54cb0c0b925686a3ec851703b74d02ee0ab
wanbaocheng <wanbaocheng1@jd.com>
2019-10-28 15:54:48 +0800

    修复加密单个文件处的一个bug。

--- py2so.py
+++ py2so.py
@@ -33,7 +33,7 @@ py2so needs the environment of python3
   -p,  --py         Python的子版本号, 默认值为 6
                     例: -p 7  (比如你使用python3.7)
   -D,  --Directory  Python项目路径 (如果使用-D参数, 将加密整个Python项目)
-  -d,  --directory  按首行是否有#py2so共六个字符作为标记进行加密，D优先于d
+  -d,  --directory  按首行是否有# py2so共七个字符作为标记进行加密，D优先于d
   -f,  --file       Python文件 (如果使用-f, 将加密单个Python文件)
   -c,  --clear      删除原文件（项目）
                     (警告: -c参数将会删除原文件, 但是为了避免损失，py2so_py3会自动备份原文件)
@@ -44,6 +44,7 @@ py2so needs the environment of python3
   python py2so.py -f test_file.py
   python py2so.py -d test_dir -m __init__.py,setup.py,[poc/,resource/,venv/,interface/] -c
     '''
+    tag_py2so = '# py2so'
     is_clear = False
     p_subv = '6'
     root_names_D = ''
@@ -51,16 +52,13 @@ py2so needs the environment of python3
     file_name = ''
     m_list = ''
     try:
-        options, args = getopt.getopt(sys.argv[1:], "vhp:Dd:f:cm:",
+        options, args = getopt.getopt(sys.argv[1:], "vhp:D:d:f:cm:",
                                       ["version", "help", "py=", "Directory=", "directory=", "file=", "clear",
-                                       "maintain=", 'nobackup'])
+                                       "maintain="])
     except getopt.GetoptError:
         print('Get options Error')
         print(help_show)
         sys.exit(1)
-
-    for key, value in options:
-        print('(key,value): ', key, value)
 
     for key, value in options:
         if key in ['-h', '--help']:
@@ -73,10 +71,8 @@ py2so needs the environment of python3
             is_clear = True
         elif key in ['-D', '--Directory']:
             root_names_D = [os.path.abspath(val.strip()) for val in value.split(',') if val.strip() is not '']
-            print(root_names_D)
         elif key in ['-d', '--directory']:
             root_names_d = [os.path.abspath(val.strip()) for val in value.split(',') if val.strip() is not '']
-            print(root_names_d)
         elif key in ['-f', '--file']:
             file_name = value
         elif key in ['-m', '--maintain']:
@@ -168,9 +164,8 @@ py2so needs the environment of python3
                                 if is_subdir(dir_pref_base, root_name_D):
                                     break
                             else:
-                                print('*: ', dir_pref)
                                 with open(dir_pref + '.py', 'r') as f:
-                                    if f.readline().find('#py2so') >= 0:
+                                    if f.readline().find(tag_py2so) >= 0:
                                         transfer(dir_pref, is_clear)
             except Exception as err:
                 print(err)
@@ -179,7 +174,7 @@ py2so needs the environment of python3
                         "Please check out the Python version You use, and use option -p to specify the definite version")
     if file_name != '':
         try:
-            dir_pref = file_name.split('.')[0]
+            dir_pref = os.path.splitext(os.path.abspath(file_name))[0]
             transfer(dir_pref, is_clear)
         except Exception as err:
             print(err)
